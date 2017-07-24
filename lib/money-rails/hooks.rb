f92b5ba72eed867ee1a58bb342bdbe53ff12da11
nim901 <nim901@users.noreply.github.com>
2017-07-24 12:53:36 -0400

    Update hooks.rb
    Override the initialize of `ActiveSupport`

--- lib/money-rails/hooks.rb
+++ lib/money-rails/hooks.rb
@@ -4,36 +4,36 @@ module MoneyRails
 
     def self.init
       # For Active Record
-      ActiveSupport.on_load(:active_record) do
-        require 'money-rails/active_model/validator'
-        require 'money-rails/active_record/monetizable'
-        ::ActiveRecord::Base.send :include, MoneyRails::ActiveRecord::Monetizable
-        if defined?(::ActiveRecord) && defined?(::ActiveRecord::VERSION)
-          if ::ActiveRecord::VERSION::MAJOR >= 4
-            rails42               = case
-                                    when ::ActiveRecord::VERSION::MAJOR < 5 && ::ActiveRecord::VERSION::MINOR >= 2
-                                      true
-                                    when ::ActiveRecord::VERSION::MAJOR >= 5
-                                      true
-                                    else
-                                      false
-                                    end
-            current_adapter = ::ActiveRecord::Base.connection_config[:adapter]
-            postgresql_with_money = rails42 && PG_ADAPTERS.include?(current_adapter)
-          end
-        end
+#       ActiveSupport.on_load(:active_record) do
+#         require 'money-rails/active_model/validator'
+#         require 'money-rails/active_record/monetizable'
+#         ::ActiveRecord::Base.send :include, MoneyRails::ActiveRecord::Monetizable
+#         if defined?(::ActiveRecord) && defined?(::ActiveRecord::VERSION)
+#           if ::ActiveRecord::VERSION::MAJOR >= 4
+#             rails42               = case
+#                                     when ::ActiveRecord::VERSION::MAJOR < 5 && ::ActiveRecord::VERSION::MINOR >= 2
+#                                       true
+#                                     when ::ActiveRecord::VERSION::MAJOR >= 5
+#                                       true
+#                                     else
+#                                       false
+#                                     end
+#             current_adapter = ::ActiveRecord::Base.connection_config[:adapter]
+#             postgresql_with_money = rails42 && PG_ADAPTERS.include?(current_adapter)
+#           end
+#         end
 
-        require "money-rails/active_record/migration_extensions/options_extractor"
-        %w{schema_statements table}.each do |file|
-          require "money-rails/active_record/migration_extensions/#{file}_pg_rails4"
-          if !postgresql_with_money
-            require "money-rails/active_record/migration_extensions/#{file}"
-          end
-        end
-        ::ActiveRecord::Migration.send :include, MoneyRails::ActiveRecord::MigrationExtensions::SchemaStatements
-        ::ActiveRecord::ConnectionAdapters::TableDefinition.send :include, MoneyRails::ActiveRecord::MigrationExtensions::Table
-        ::ActiveRecord::ConnectionAdapters::Table.send :include, MoneyRails::ActiveRecord::MigrationExtensions::Table
-      end
+#         require "money-rails/active_record/migration_extensions/options_extractor"
+#         %w{schema_statements table}.each do |file|
+#           require "money-rails/active_record/migration_extensions/#{file}_pg_rails4"
+#           if !postgresql_with_money
+#             require "money-rails/active_record/migration_extensions/#{file}"
+#           end
+#         end
+#         ::ActiveRecord::Migration.send :include, MoneyRails::ActiveRecord::MigrationExtensions::SchemaStatements
+#         ::ActiveRecord::ConnectionAdapters::TableDefinition.send :include, MoneyRails::ActiveRecord::MigrationExtensions::Table
+#         ::ActiveRecord::ConnectionAdapters::Table.send :include, MoneyRails::ActiveRecord::MigrationExtensions::Table
+#       end
 
       # For Mongoid
       begin; require 'mongoid'; require 'mongoid/version'; rescue LoadError; end
